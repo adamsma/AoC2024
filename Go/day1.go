e34a99f371a53417336d9ea519f5b0c82bf44b7c
Marcus Adams <adamsma@users.noreply.github.com>
2025-04-14 01:22:37 -0400

    Refactor to encapsulate day specific functions

--- Go/day1.go
+++ Go/day1.go
@@ -11,9 +11,9 @@ import (
 )
 
 func Day1() (int, int, int, int) {
-  
-  const samplePath = "../Data/Sample_Day1.txt"
-  const inputPath = "../Data/Day1.txt"
+
+	const samplePath = "../Data/Sample_Day1.txt"
+	const inputPath = "../Data/Day1.txt"
 
 	sFile, err := os.Open(samplePath)
 	if err != nil {
@@ -27,75 +27,82 @@ func Day1() (int, int, int, int) {
 	}
 	defer file.Close()
 
-	l1, l2 := CreateLists(sFile)
-	s1 := part1Calc(l1, l2)
-	s2 := part2Calc(l1, l2)
+	l1, l2 := D1.CreateLists(sFile)
+	s1 := D1.part1Calc(l1, l2)
+	s2 := D1.part2Calc(l1, l2)
 
-	l1, l2 = CreateLists(file)
-	p1 := part1Calc(l1, l2)
-	p2 := part2Calc(l1, l2)
+	l1, l2 = D1.CreateLists(file)
+	p1 := D1.part1Calc(l1, l2)
+	p2 := D1.part2Calc(l1, l2)
 
 	return s1, p1, s2, p2
 
 }
 
-func CreateLists(f *os.File) ([]int, []int) {
+var D1 = struct {
+	CreateLists func(*os.File) ([]int, []int)
+	part1Calc   func([]int, []int) int
+	part2Calc   func([]int, []int) int
+}{
 
-	scanner := bufio.NewScanner(f)
-	var l1, l2 []int
+	CreateLists: func(f *os.File) ([]int, []int) {
 
-	for scanner.Scan() {
-		cols := strings.SplitN(scanner.Text(), "   ", 2)
+		scanner := bufio.NewScanner(f)
+		var l1, l2 []int
 
-		i, _ := strconv.Atoi(cols[0])
-		l1 = append(l1, i)
-		i, _ = strconv.Atoi(cols[1])
-		l2 = append(l2, i)
-	}
+		for scanner.Scan() {
+			cols := strings.SplitN(scanner.Text(), "   ", 2)
 
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
-	}
-
-	return l1, l2
-
-}
-
-// Calculate total distances between list
-func part1Calc(l1, l2 []int) int {
-
-	slices.Sort(l1)
-	slices.Sort(l2)
-	var dist float64
-
-	for i, val := range l1 {
-		dist += math.Abs(float64(val - l2[i]))
-	}
-
-	return int(dist)
-
-}
-
-// Calculate similarity score
-func part2Calc(l1, l2 []int) int {
-
-	counts := make(map[int]int)
-	sim := 0
-
-	for _, val := range l1 {
-		counts[val] = 0
-	}
-
-	for _, val := range l2 {
-		if _, ok := counts[val]; ok {
-			counts[val] += 1
+			i, _ := strconv.Atoi(cols[0])
+			l1 = append(l1, i)
+			i, _ = strconv.Atoi(cols[1])
+			l2 = append(l2, i)
 		}
-	}
 
-	for _, val := range l1 {
-		sim += val * counts[val]
-	}
+		if err := scanner.Err(); err != nil {
+			log.Fatal(err)
+		}
 
-	return sim
+		return l1, l2
 
+	},
+
+	// Calculate total distances between list
+	part1Calc: func(l1, l2 []int) int {
+
+		slices.Sort(l1)
+		slices.Sort(l2)
+		var dist float64
+
+		for i, val := range l1 {
+			dist += math.Abs(float64(val - l2[i]))
+		}
+
+		return int(dist)
+
+	},
+
+	// Calculate similarity score
+	part2Calc: func(l1, l2 []int) int {
+
+		counts := make(map[int]int)
+		sim := 0
+
+		for _, val := range l1 {
+			counts[val] = 0
+		}
+
+		for _, val := range l2 {
+			if _, ok := counts[val]; ok {
+				counts[val] += 1
+			}
+		}
+
+		for _, val := range l1 {
+			sim += val * counts[val]
+		}
+
+		return sim
+
+	},
 }
