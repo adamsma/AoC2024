6bd7ade99dec0aed49e2cbd46e380234c3953003
Marcus Adams <adamsma@users.noreply.github.com>
2025-04-17 23:38:14 -0400

    refactor to use let instead of var

--- Node/Day1.js
+++ Node/Day1.js
@@ -9,7 +9,7 @@ const CreateLists = (data) => {
 
   const rows = data.trim().split("\r\n")
   rows.forEach((row) => {
-    var [x, y] = row.split("   ").slice(0, 2)
+    let [x, y] = row.split("   ").slice(0, 2)
     l1.push(parseInt(x))
     l2.push(parseInt(y))
   })
@@ -19,7 +19,7 @@ const CreateLists = (data) => {
 
 // Calculate total distances between list
 const Part1Calc = (l1, l2) => {
-  var dist = 0
+  let dist = 0
 
   l1.forEach((x, i) => {
     dist += Math.abs(x - l2[i])
@@ -31,7 +31,7 @@ const Part1Calc = (l1, l2) => {
 // Calculate similarity score
 const Part2Calc = (l1, l2) => {
   const counts = new Map()
-  var sim = 0
+  let sim = 0
 
   l1.forEach((x) => counts.set(x, 0))
   l2.forEach((x) => {
@@ -46,16 +46,16 @@ const Part2Calc = (l1, l2) => {
 const sData = fs.readFileSync(samplePath, "utf8")
 const data = fs.readFileSync(inputPath, "utf8")
 
-var [l1, l2] = CreateLists(sData)
-l1.sort()
-l2.sort()
-var s1 = Part1Calc(l1, l2)
-var s2 = Part2Calc(l1, l2)
+let [sl1, sl2] = CreateLists(sData)
+sl1.sort()
+sl2.sort()
+let s1 = Part1Calc(sl1, sl2)
+let s2 = Part2Calc(sl1, sl2)
 
-var [l1, l2] = CreateLists(data)
+let [l1, l2] = CreateLists(data)
 l1.sort()
 l2.sort()
-var p1 = Part1Calc(l1, l2)
-var p2 = Part2Calc(l1, l2)
+let p1 = Part1Calc(l1, l2)
+let p2 = Part2Calc(l1, l2)
 
 module.exports = { s1, s2, p1, p2 }
--- Node/Day2.js
+++ Node/Day2.js
@@ -4,7 +4,7 @@ const samplePath = "../data/Sample_Day2.txt"
 const inputPath = "../data/Day2.txt"
 
 const ParseReports = (data) => {
-  var reports = []
+  let reports = []
   const rows = data.trim().split("\r\n")
   rows.forEach((row) => {
     reports.push(row.split(" ").map((x) => parseInt(x)))
@@ -14,7 +14,7 @@ const ParseReports = (data) => {
 }
 
 const IsSafe = (report) => {
-  var delta = []
+  let delta = []
   for (i = 1; i < report.length; i++) {
     delta.push(report[i] - report[i - 1])
   }
@@ -23,7 +23,7 @@ const IsSafe = (report) => {
     return false
   }
 
-  var lastSign = Math.sign(delta[0])
+  let lastSign = Math.sign(delta[0])
   for (i = 1; i < delta.length; i++) {
     if (delta[i] === 0) {
       return false
@@ -44,7 +44,7 @@ const IsDampenSafe = (report) => {
   }
 
   // drop elements one by one to see if any make it safe
-  for (var i = 0; i < report.length; i++) {
+  for (let i = 0; i < report.length; i++) {
     if (IsSafe(report.toSpliced(i, 1))) {
       return true
     }
@@ -54,13 +54,13 @@ const IsDampenSafe = (report) => {
 }
 
 const Part1Calc = (reports) => {
-  var sum = 0
+  let sum = 0
   reports.forEach((r) => (sum += IsSafe(r) ? 1 : 0))
   return sum
 }
 
 const Part2Calc = (reports) => {
-  var sum = 0
+  let sum = 0
   reports.forEach((r) => (sum += IsDampenSafe(r) ? 1 : 0))
   return sum
 }
@@ -68,12 +68,12 @@ const Part2Calc = (reports) => {
 const sData = fs.readFileSync(samplePath, "utf8")
 const data = fs.readFileSync(inputPath, "utf8")
 
-var reports = ParseReports(sData)
-var s1 = Part1Calc(reports)
-var s2 = Part2Calc(reports)
+let sReports = ParseReports(sData)
+let s1 = Part1Calc(sReports)
+let s2 = Part2Calc(sReports)
 
-var reports = ParseReports(data)
-var p1 = Part1Calc(reports)
-var p2 = Part2Calc(reports)
+let reports = ParseReports(data)
+let p1 = Part1Calc(reports)
+let p2 = Part2Calc(reports)
 
 module.exports = { s1, s2, p1, p2 }
